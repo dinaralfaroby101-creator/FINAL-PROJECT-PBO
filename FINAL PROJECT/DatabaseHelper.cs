5696740a3df11964afd01215eb51eda92709ebdc
Madtalfar ahmad <dinaralfaroby101@gmail.com>
2026-05-19 09:49:49 +0700

    Add project files.

---
+++ FINAL PROJECT/DatabaseHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Npgsql;
+using System.Threading.Tasks;
+
+namespace FINAL_PROJECT
+{
+    static class DatabaseHelper
+    {
+        private static string connectionString =
+            "Host=localhost;Port=5432;Database=Final_project_PBO;Username=postgres;Password=dinarjosjis";
+        public static NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(connectionString);
+    }
+    }
+}
